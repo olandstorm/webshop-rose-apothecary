de7470cbb5cee602cae412716ae42e60adf934ee
olandstorm <100365091+olandstorm@users.noreply.github.com>
2023-11-20 18:38:58 +0100

    :lipstick: fixat mer color-theme och fixat frasbild på första sidan

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ const themeToggle = document.querySelector('#toggleTheme');
 const lightMode = document.querySelector('#lightModeIcon');
 const darkMode = document.querySelector('#darkModeIcon');
 
+const phraseImgBlack = document.querySelector('#phraseImageBlack');
+const phraseImgWhite = document.querySelector('#phraseImageWhite');
+
 // Funktion för att öppna och stänga navigationsmenyn, även med länkarna
 function toggleMenu() {
   const isOpen = nav.classList.toggle('open');
@@ -41,8 +44,17 @@ menuLinks.forEach((item) => {
 // Event för menyknapp
 menuBtn.addEventListener('click', toggleMenu);
 
-// Toggle för dark/lightmode
+// Funktioner för att byta bild på framsidan beroende på theme
+function changePhraseToWhite() {
+  phraseImgWhite.classList.remove('visually_hidden');
+  phraseImgBlack.classList.add('visually_hidden');
+}
+function changePhraseToBlack() {
+  phraseImgWhite.classList.add('visually_hidden');
+  phraseImgBlack.classList.remove('visually_hidden');
+}
 
+// Toggle för dark/lightmode
 if (
   // eslint-disable-next-line
   window.matchMedia &&
@@ -50,14 +62,22 @@ if (
 ) {
   document.body.classList.add('dark_mode');
   darkMode.classList.toggle('visually_hidden');
+  changePhraseToWhite();
 } else {
   lightMode.classList.toggle('visually_hidden');
+  changePhraseToBlack();
 }
 
 function toggleTheme() {
   document.body.classList.toggle('dark_mode');
   lightMode.classList.toggle('visually_hidden');
   darkMode.classList.toggle('visually_hidden');
+  const isDark = document.body.classList.contains('dark_mode');
+  if (isDark) {
+    changePhraseToWhite();
+  } else {
+    changePhraseToBlack();
+  }
 }
 
 themeToggle.addEventListener('click', toggleTheme);
